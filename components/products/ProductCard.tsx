f507ee871f642b9246eaeb5ec1ca15ead441c2fa
agus022 <flores.jorge.1j@gmail.com>
2024-06-05 10:24:01 -0600

    ready-admin-edit-product-search-product

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { products } from "@/prisma/data/products"
-import { formatCurrency } from "@/src/utils"
+import { formatCurrency, getImagePath } from "@/src/utils"
 import { Product } from "@prisma/client"
 import Image from "next/image"
 import AddProductButton from "./AddProductButton"
@@ -10,13 +10,14 @@ type ProductCardProps ={
 
 
 export default function ProductCard({product}: ProductCardProps){
+    const imagePath = getImagePath(product.image)
     return (
         <div className="border bg-white">
 
             <Image 
                 width={400}
                 height={500}
-                src={`/products/${product.image}.jpg`}
+                src={imagePath}
                 alt={`Imagen Platillo ${product.name}`}
                 quality={85}
             />
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -4,3 +4,11 @@ export function formatCurrency(cant: number){
         currency: 'USD'
     }).format(cant)
 }
+export function getImagePath(imagePath: string) {
+  const cloudinaryBaseUrl = "https://res.cloudinary.com";
+  if (imagePath.startsWith(cloudinaryBaseUrl)) {
+    return imagePath;
+  } else {
+    return `/products/${imagePath}.jpg`;
+  }
+}
