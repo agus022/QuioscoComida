aa1488ac81e6c248802436342717909f112cedf3
agus022 <flores.jorge.1j@gmail.com>
2024-06-05 19:49:56 -0600

    last-commit-finished-orders

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,35 +1,37 @@
 "use client";
 import useSWR from "swr";
-import OrderCard from "@/components/order/OrderCard";
-import Heading from "@/components/ui/Heading";
+import Logo from "@/components/ui/Logo";
 import { OrderWithProducts } from "@/src/types";
+import LatestOrderItem from "@/components/order/LatestOrderItem";
 
 export default function OrdersPage() {
-  const url = "/admin/orders/api";
+  const url = "/orders/api";
   const fetcher = () =>
     fetch(url)
       .then((res) => res.json())
       .then((data) => data);
   const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
-    refreshInterval: 60000,
+    refreshInterval: 1000,
     revalidateOnFocus: false,
   });
-
   if (isLoading) return <p>Cargando...</p>;
-
   if (data)
     return (
       <>
-        <Heading>Administrar Ordenes</Heading>
+        <h1 className="text-center mt-20 text-6xl font-black">
+          Ordenes Listas
+        </h1>
+
+        <Logo />
 
         {data.length ? (
-          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          <div className="grid grid-cols-2 gap-5 max-w-5xl mx-auto mt-10">
             {data.map((order) => (
-              <OrderCard key={order.id} order={order} />
+              <LatestOrderItem key={order.id} order={order} />
             ))}
           </div>
         ) : (
-          <p className="text-center">No hay ordenes Pendientes</p>
+          <p className="text-center my-10">No hay ordenes listas</p>
         )}
       </>
     );
