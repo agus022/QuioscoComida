90ebc8798cc08e75fd79da837f0bd293e27eba33
agus022 <flores.jorge.1j@gmail.com>
2024-06-05 18:10:56 -0600

    final-app

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-
+import { redirect } from "next/navigation";
 export default function Home() {
-  return <h1>Hola mundo desde Next.js</h1>;
+  redirect("/order/cafe");
 }
